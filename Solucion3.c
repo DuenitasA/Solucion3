79f769eff9ccc1ebfc498450960e2216e54b5018
Alejandro Duenas <alejandro.duenas@udla.edu.ec>
2024-01-28 14:07:50 -0500

    Estructura de los datos que se van a utilizar

---
+++ Solucion3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+struct OrdenTrabajo
+{
+    char nombreCliente[25];
+    char apellidoCliente[25];
+    char trabajo[50];
+    float costo;
+    int Factura;
+
+};
+
+/* Se creo una estructura que almacene todos los datos que se van a imprimir proximamente en un archivo plano*/
+
+int main(void){
+
+
+
+
+
+
+    return 0;
+}
